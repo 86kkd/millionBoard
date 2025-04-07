4c7ef2be8bf7d2102df876db21827ab3b75970df
86kkd <2973720373@qq.com>
2025-04-07 23:29:34 +0800

    success config

---
+++ src/skateboard_foc_project/components/communication/test/tests/communication_test.c
@@ -0,0 +1,12 @@
+#include "communication.h"
+#include "unity.h"
+#include <stdio.h>
+
+void app_main(void) {
+  UNITY_BEGIN();
+
+  // Add your test cases here
+  // Example: TEST_ASSERT_EQUAL_INT(0, some_function());
+
+  UNITY_END();
+}
---
+++ src/skateboard_foc_project/components/utils/test/tests/utils_test.c
@@ -0,0 +1,12 @@
+#include "unity.h"
+#include "utils.h"
+#include <stdio.h>
+
+void app_main(void) {
+  UNITY_BEGIN();
+
+  // Add your test cases here
+  // Example: TEST_ASSERT_EQUAL_INT(0, some_function());
+
+  UNITY_END();
+}
